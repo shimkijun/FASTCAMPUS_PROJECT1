59face52bd90de6682c54cec64d73d3c80968691
shimkijun <shimkijun@naver.com>
2020-01-15 20:59:48 +0900

    JPA , MySQL , CRUD test code

--- src/test/java/com/example/admin_project/repository/UserRepositoryTest.java
+++ src/test/java/com/example/admin_project/repository/UserRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.example.admin_project.repository;
 
 import com.example.admin_project.model.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootTest
 public class UserRepositoryTest {
@@ -26,16 +29,45 @@ public class UserRepositoryTest {
         System.out.println(newUser);
     }
 
+    @Test
     void read(){
+        Optional<User> user = userRepository.findById(10L);
 
+        user.ifPresent(selectUser ->{
+            System.out.println("user : " + selectUser );
+        });
     }
 
+    @Test
+    @Transactional
     public void update(){
 
+        // findById 를 기준으로 update 함
+        Optional<User> user = userRepository.findById(10L);
+
+        user.ifPresent(selectUser ->{
+           selectUser.setAccount("pppp");
+           selectUser.setUpdatedAt(LocalDateTime.now());
+           selectUser.setUpdatedBy("update method");
+
+           userRepository.save(selectUser);
+        });
     }
 
+    @Test
+    @Transactional  // 메소드는 모두 실행하지만 실제 데이터베이스의 데이터는 남아있음 = Roll back
     public void delete(){
+        Optional<User> user = userRepository.findById(11L);
 
+        Assertions.assertTrue(user.isPresent()); // true
+
+        user.ifPresent(selectUser ->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(11L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); // false
     }
 
 }
